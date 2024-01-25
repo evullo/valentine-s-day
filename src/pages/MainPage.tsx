32ea732724f26b3b4529a7e825929477c55cf0b3
Enzo Vullo <e.vullo@student.helmo.be>
2024-01-25 19:20:51 +0100

    MainPage correction

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -15,6 +15,9 @@ import chapi23 from "../assets/images/chapi23.jpeg";
 import newYear23 from "../assets/images/newYear23.jpeg";
 
 const MainPage = () => {
+    const commonDivStyle : string = "absolute z-10 flex flex-nowrap items-center";
+    const commonImgStyle : string = "relative w-1/3";
+
     return (
         <main className="relative w-fit mx-auto">
             <div>
@@ -24,49 +27,49 @@ const MainPage = () => {
 
             <img className="px-10 mb-10 md:mx-auto" src={line} alt="long line from top to bottom"/>
 
-            <div className="absolute z-10 flex flex-nowrap items-center top-[6%]">
+            <div className={`${commonDivStyle} top-[6%]`}>
                 <Paragraph>Notre diplôme de rhétos en 2021</Paragraph>
-                <StackedCard className="relative w-1/3 mr-5" src={rheto21}/>
+                <StackedCard className={`${commonImgStyle} mr-5`} src={rheto21}/>
             </div>
 
-            <div className="absolute z-10 flex flex-nowrap items-center top-[15%]">
-                <StackedCard className="relative w-1/3 ml-5" src={bruges21}/>
+            <div className={`${commonDivStyle} top-[15%]`}>
+                <StackedCard className={`${commonImgStyle} ml-5`} src={bruges21}/>
                 <Paragraph>Voyage à Bruges en famille</Paragraph>
             </div>
 
-            <div className="absolute z-10 flex flex-nowrap items-center top-[24%]">
+            <div className={`${commonDivStyle} top-[24%]`}>
                 <Paragraph>Tes 18 ans, enfin majeure</Paragraph>
-                <StackedCard className="relative w-1/3 mr-5" src={birthday21}/>
+                <StackedCard className={`${commonImgStyle} mr-5`} src={birthday21}/>
             </div>
 
-            <div className="absolute z-10 flex flex-nowrap items-center top-[35%]">
-                <StackedCard className="relative w-1/3 ml-5" src={noel21}/>
+            <div className={`${commonDivStyle} top-[35%]`}>
+                <StackedCard className={`${commonImgStyle} ml-5`} src={noel21}/>
                 <Paragraph>Noël 2021 sur le marché</Paragraph>
             </div>
 
-            <div className="absolute z-10 flex flex-nowrap items-center top-[42%]">
+            <div className={`${commonDivStyle} top-[42%]`}>
                 <Paragraph>Nouvel an 2022 en famille</Paragraph>
-                <StackedCard className="relative w-1/3 mr-5" src={newYear22}/>
+                <StackedCard className={`${commonImgStyle} mr-5`} src={newYear22}/>
             </div>
 
-            <div className="absolute z-10 flex flex-nowrap items-center top-[51%]">
-                <StackedCard className="relative w-1/3 ml-5" src={sicily22}/>
+            <div className={`${commonDivStyle} top-[51%]`}>
+                <StackedCard className={`${commonImgStyle} ml-5`} src={sicily22}/>
                 <Paragraph>Notre voyage en Sicile en famille</Paragraph>
             </div>
 
-            <div className="absolute z-10 flex flex-nowrap items-center top-[62%]">
+            <div className={`${commonDivStyle} top-[62%]`}>
                 <Paragraph>Premier long voyage ensemble au Canada</Paragraph>
-                <StackedCard className="relative w-1/3 mr-5" src={canada23}/>
+                <StackedCard className={`${commonImgStyle} mr-5`} src={canada23}/>
             </div>
 
-            <div className="absolute z-10 flex flex-nowrap items-center top-[73%]">
+            <div className={`${commonDivStyle} top-[73%]`}>
                 <StackedCard className="relative w-2/3 ml-5" src={chapi23}/>
                 <Paragraph>Les chapis c'est quand même important</Paragraph>
             </div>
 
-            <div className="absolute z-10 flex flex-nowrap items-center top-[80%]">
+            <div className={`${commonDivStyle} top-[80%]`}>
                 <Paragraph>Et pour finir,<br/>notre dernier nouvel an</Paragraph>
-                <StackedCard className="relative w-1/3 mr-5" src={newYear23}/>
+                <StackedCard className={`${commonImgStyle} mr-5`} src={newYear23}/>
             </div>
 
             <Form/>
